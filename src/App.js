8397c8d5a82b36006f77a15e77a699956f72677b
Duy Linh <doretienshinh2@gmail.com>
2021-05-27 00:41:25 +0700

    done session 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Expenses from './components/Expenses';
+import React from 'react';
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: 'e1',
